c7920b09d75f75618633b2fb5b7a944b8fff4180
Rafael Ioshi <rafa.ioshi@gmail.com>
2026-06-19 15:57:25 -0300

    terminal tab

--- lua/config/plugins/fterm.lua
+++ lua/config/plugins/fterm.lua
@@ -12,8 +12,5 @@ return {
 
         -- Keymaps
         vim.keymap.set('n', '<leader><C-i>', '<CMD>lua require("FTerm").toggle()<CR>')
-        vim.keymap.set('t', '<leader><C-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-        -- Tab dentro do terminal
-        vim.api.nvim_set_keymap('t', '<Tab>', '<Tab>', { noremap = true, silent = true })
     end
 }
